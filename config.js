8749732f694de43eebb84d08e78f222c3343440e
NanzBot <kentud127@gmail.com>
2022-09-20 23:24:44 +0700

    forsub

---
+++ config.js
@@ -0,0 +1,57 @@
+/**
+   * Create By THE JO BOT
+   * Base by Dika Ardnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': 'Your Key',
+}
+
+
+global.ownername = 'Shinzi Bot War'
+global.autorecording = false //status auto merekam ( auto record )
+global.autoketik = false //status auto mengetik (auto typing)
+global.available = false //status online (online)
+// Other
+global.owner = ['6285781512191','6285781512191','6285781512191']
+global.premium = ['60177426853']
+global.packname = 'The Shinzi'
+global.author = 'WhatsApp Bot'
+global.wlcm = []
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Sukses Kids',
+    admin: 'Fitur Khusus Admin Grup Dekkk!',
+    botAdmin: 'Jadjin Gwech Admin Dulu dek!',
+    owner: 'Fitur Khusus Orang Teladan',
+    group: 'Pakenya Digrup Dekk Hadeh!',
+    private: 'Pakenya Di Private Dekk Hadeh!',
+    bot: 'Cmn Gwech Yang Bisa Pake kids',
+    wait: 'Utiwi...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/jo.jpg')
+global.visoka = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
